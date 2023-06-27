6c0fb57079e05db057da6f2f995bc6de0ec9f024
Ajay Kumar <102378038+ajaykumar2pp@users.noreply.github.com>
2023-06-27 16:33:03 +0530

    direct link bg image

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -7,7 +7,7 @@ const TeamPage = () => {
   return (
   <>
   <Navbar />
-    <section className="bg-center bg-no-repeat bg-[url('../../public/Yoga-5.jpg')] bg-cover  bg-gray-600 bg-blend-multiply">
+    <section className="bg-center bg-no-repeat bg-[url('https://i.ibb.co/YXJ25wL/Yoga-5.jpg')] bg-cover  bg-gray-600 bg-blend-multiply">
     <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
         Our Team</h1>
